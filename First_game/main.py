aa16b9d7f8cc00ace29fcf4fb17d5950015f3021
dimon-ton <saichon2namoom@gmail.com>
2023-03-10 21:46:52 +0700

    EP.4 create enemy and player are  shooting with the bullet complete learning.

--- First_game/main.py
+++ First_game/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # start pygame
 pygame.init()
@@ -9,7 +10,7 @@ FPS = 30
 WIDTH = 800
 HEIGHT = 700
 
-# set the color
+# set the color RGB
 BLACK = (0,0,0)
 GREEN = (0, 255, 0)
 
@@ -25,7 +26,7 @@ clock = pygame.time.Clock()
 
 
 
-class Player(pygame.sprite.Sprite):
+class Enemy(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -40,18 +41,109 @@ class Player(pygame.sprite.Sprite):
 
         # create rectangle
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+
+        # random x axis
+        rand_x = random.randint(self.rect.width, WIDTH - self.rect.width)
+
+        # position from the center of sprite
+        self.rect.center = (rand_x, 0)
+
+        # speed y
+        self.speed_y = random.randint(2, 5)
 
     def update(self):
-        self.rect.y += 5
+        self.rect.y += self.speed_y
         if self.rect.bottom > HEIGHT:
             self.rect.y = 0
+
+            # random x again
+            rand_x = random.randint(self.rect.width, WIDTH - self.rect.width)
+            self.rect.x = rand_x
+            self.speed_y = random.randint(2, 5)
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        img = 'First_game/bomber.png'
+        self.image = pygame.image.load(img).convert_alpha()
+        
+        
+        # self.image = pygame.Surface((50, 50))
+        # self.image.fill(GREEN)
+
+       
+
+        # create rectangle
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT - self.rect.height)
+
+        # speed x
+        self.speed_x = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+
+
+    def update(self):
+        # self.rect.y += 5
+
+        self.speed_x = 0
+        # check if the key is pressed.
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speed_x = -5
+
+        if keystate[pygame.K_RIGHT]:
+            self.speed_x = 5
+
+        self.rect.x += self.speed_x
+
+
+        if self.rect.bottom > HEIGHT:
+            self.rect.y = 0
+
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self,x ,y):
+        # x = center of the airplane
+        # y = get the top of the air plane
+        pygame.sprite.Sprite.__init__(self)
+       
+        
+        self.image = pygame.Surface((10, 10))
+        self.image.fill(GREEN)
+
+       
+
+        # create rectangle
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+
+        # speed x
+        self.speed_y = -10
+
+    def update(self):
+        self.rect.y += self.speed_y
+
+        # delete bullet when the y axis is less than 0.
+        if self.rect.y < 0:
+            self.kill()
+
 
 
 # create group of sprites
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+
+# enemy
+for i in range(3):
+    enemy = Enemy()
+    all_sprites.add(enemy)
 
 # the status of game
 running = True
@@ -64,6 +156,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     all_sprites.update()
     
