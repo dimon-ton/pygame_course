babed258b6e69b6dc828da178f7c0a053d5eafbd
dimon-ton <saichon2namoom@gmail.com>
2024-06-06 22:12:58 +0700

    EP.10 reach at 01:07:53

--- pongclass/tutorial.py
+++ pongclass/tutorial.py
@@ -1,6 +1,5 @@
 import pygame, sys
-
-
+import random
 
 pygame.init()
 
@@ -8,9 +7,18 @@ clock = pygame.time.Clock()
 
 
 # game variable
-ball_speed_x = 5
-ball_speed_y = 5
+ball_speed_x = 5 * random.choice((1, -1))
+ball_speed_y = 5 * random.choice((1, -1))
+player_speed = 0
+computer_speed = 5
 
+
+player_score = 0
+computer_score = 0
+
+game_font = pygame.font.Font('AnonymousPro-Regular.ttf', 30)
+
+# ball animation if the ball is collide with the border of screen then it reflex
 def ball_anim():
     global ball_speed_y, ball_speed_x
 
@@ -22,11 +30,40 @@ def ball_anim():
         ball_speed_y *= -1
 
     if ball.left <=0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        # ball_speed_x *= -1
+        ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(computer):
         ball_speed_x *= -1
 
+
+def player_anim():
+    player.y += player_speed
+
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+
+def computer_logic():
+    if computer.top < ball.y:
+        computer.y += computer_speed
+    if computer.bottom > ball.y:
+        computer.y -= computer_speed
+
+
+    if computer.top <= 0:
+        computer.top = 0
+    if computer.bottom >= screen_height:
+        computer.bottom = screen_height
+
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width / 2, screen_height / 2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
 
 # screen surface setup
 screen_width = 1280
@@ -50,10 +87,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_speed -= 5
+            if  event.key == pygame.K_DOWN:
+                player_speed += 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player_speed += 5
+            if event.key == pygame.K_DOWN:
+                player_speed -=5
+            
 
 
     # game play loop
     ball_anim()
+    player_anim()
+    computer_logic()
+
 
     screen.fill(bg_color)
     pygame.draw.rect(screen, white, player)
@@ -61,6 +112,13 @@ while True:
     pygame.draw.ellipse(screen, red, ball)
     pygame.draw.aaline(screen, white, (screen_width / 2, 0), (screen_width / 2, screen_height))
 
+
+    player_text = game_font.render(f'{player_score}', True, white)
+    screen.blit(player_text, ((660, 485)))
+
+
+    computer_text = game_font.render(f'{computer_score}', True, white)
+    screen.blit(computer_text, ((600, 485)))
 
     pygame.display.update()
     clock.tick(60)
