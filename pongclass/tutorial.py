7fe5dfa78058d6c91bdc7c15a3f4e91b1fc1c214
dimon-ton <saichon2namoom@gmail.com>
2024-09-09 18:50:20 +0700

    add settings to pong game finish

--- pongclass/tutorial.py
+++ pongclass/tutorial.py
@@ -24,7 +24,7 @@ computer_score = 0
 
 
 # sound effect
-
+sound_enabled = True
 plop_sound = pygame.mixer.Sound('ping_pong_8bit_plop.ogg')
 peep_sound = pygame.mixer.Sound('ping_pong_8bit_peep.ogg')
 beep_sound = pygame.mixer.Sound('ping_pong_8bit_beeep.ogg')
@@ -61,14 +61,21 @@ def ball_anim():
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
-        # pygame.mixer.Sound.play(plop_sound)
-        pygame.mixer.Sound.play(random.choice((plop_sound, peep_sound)))
+        if sound_enabled:
+
+            # pygame.mixer.Sound.play(plop_sound)
+            pygame.mixer.Sound.play(random.choice((plop_sound, peep_sound)))
+
+
         ball_speed_y *= -1
 
     # player score
     if ball.left <=0:
         player_score += 1
-        pygame.mixer.Sound.play(beep_sound)
+
+        if sound_enabled:
+            pygame.mixer.Sound.play(beep_sound)
+
         ball_restart()
         
     if ball.right >= screen_width:
@@ -77,7 +84,9 @@ def ball_anim():
         ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(computer):
-        pygame.mixer.Sound.play(plop_sound)
+        if sound_enabled:
+            pygame.mixer.Sound.play(plop_sound)
+
         ball_speed_x *= -1
 
 
@@ -135,8 +144,38 @@ def drawgame_layout():
     screen.blit(computer_text, ((600, 485)))
 
 
+def toggle_sound():
+    global sound_enabled
+
+    if sound_enabled:
+        sound_enabled = False
+        print('sound off')
+    else:
+        sound_enabled = True
+        print('sound on')
+
+
+
+def change_game_diff(value, difficulty):
+    '''
+        function to change the difficulty of the game
+    '''
+
+    global computer_speed
+    
+    if value[0][0] == 'Easy':
+        computer_speed = difficulty
+    elif value[0][0] == 'Medium':
+        computer_speed = difficulty
+    elif value[0][0] == 'Hard':
+        computer_speed = difficulty
+
+    
+
 class Game:
     def __init__(self):
+        global computer_speed
+
         pygame.init()
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         pygame.display.set_caption("Pong Game")  
@@ -144,7 +183,7 @@ class Game:
         pygame.display.set_icon(game_icon)
     
     def single_player(self):
-        global player_speed, player_text, computer_text
+        global player_speed, player_text, computer_text, computer_speed
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -172,6 +211,7 @@ class Game:
             drawgame_layout()
             computer_logic()
 
+    
             # update the windows
             pygame.display.update()
             clock.tick(60)
@@ -222,18 +262,29 @@ class Game:
             clock.tick(60)
 
 
-
-
+def exit_game(self):
+    pygame.quit()
+    sys.exit()
 
 game = Game()
 
+settings_menu = pygame_menu.Menu('Game Settings', screen.get_width(), screen.get_height(), theme=themes.THEME_GREEN)
+audio = settings_menu.add.button("Toggle Sound: ON/OFF", toggle_sound)
 
+difficulty = settings_menu.add.selector(
+    title = 'Select Difficulty: ',
+    items= [('Easy', 3), ('Medium', 5), ('Hard', 7)],
+    onchange=change_game_diff,
+    selector_id='selector_difficulty'
 
-
+)
 
 menu = pygame_menu.Menu('Welcome to Pong Game', screen.get_width(), screen.get_height(), theme=themes.THEME_BLUE)
 menu.add.button('Single Player', game.single_player)
 menu.add.button('Two player', game.two_player)
+menu.add.button('Game Settings', settings_menu)
+menu.add.button('Quit Game', exit_game)
+
 
 menu.mainloop(screen)
 
